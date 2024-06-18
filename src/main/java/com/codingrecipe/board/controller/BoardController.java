5653d8a2acf50be68a1e67161162b49e04e59efe
lew663 <leunwoo000@gmail.com>
2024-06-18 09:55:58 +0900

    2. 글작성

--- src/main/java/com/codingrecipe/board/controller/BoardController.java
+++ src/main/java/com/codingrecipe/board/controller/BoardController.java
@@ -1,14 +1,28 @@
 package com.codingrecipe.board.controller;
 
+import com.codingrecipe.board.dto.BoardDTO;
+import com.codingrecipe.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/board")
 public class BoardController {
+  private final BoardService boardService;
+
   @GetMapping("/save")
-  public String saveForm(){
+  public String saveForm() {
     return "save";
+  }
+
+  @PostMapping("/save")
+  public String save(@ModelAttribute BoardDTO boardDTO) {
+    boardService.save(boardDTO);
+    return "index";
   }
 }
---
+++ src/main/java/com/codingrecipe/board/dto/BoardDTO.java
@@ -0,0 +1,21 @@
+package com.codingrecipe.board.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor // 기본생성자
+@AllArgsConstructor // 모든 필드를 매개변수로 하는 생성자
+public class BoardDTO {
+  private Long id;
+  private String boardWriter;
+  private String boardPass;
+  private String boardTitle;
+  private String boardContents;
+  private int boardHits; // 조회수
+  private LocalDateTime boardCreatedTime;
+  private LocalDateTime boardUpdatedTime;
+}
